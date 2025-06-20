3bd29a8e126006b9d06df8bde50a7cb26d63d868
lovin4ik <122011576+lovin4ik@users.noreply.github.com>
2025-06-20 19:18:29 +0300

    upgrade

--- src/shared/ui/codePreview.ts
+++ src/shared/ui/codePreview.ts
@@ -4,16 +4,10 @@ import { twMerge } from 'tailwind-merge'
 interface Props {
 	lang?: string
 	className?: string
-	fileName?: string
 	code: string
 }
 
-export async function codePreview({
-	lang = 'js',
-	className,
-	fileName,
-	code
-}: Props) {
+export async function codePreview({ lang = 'js', className, code }: Props) {
 	const div = document.createElement('div') as HTMLElement
 
 	div.className = twMerge(
@@ -27,7 +21,7 @@ export async function codePreview({
 
 	div.innerHTML = `
 		<div class="absolute top-0 left-0 right-0 flex flex-row items-center justify-between px-4 py-2">
-			<span class="text-sm text-white/60 absolute right-2 top-2">${fileName}.${lang}</span>
+			<span class="text-sm text-white/60 absolute right-2 top-2">${lang}</span>
 		</div>
 		${htmlCode}
 	`
